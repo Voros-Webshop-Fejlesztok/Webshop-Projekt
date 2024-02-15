b8c877318dbe802691b17e191aa0109d3f53bfe9
kiss-csongor04 <kisscsongor@students.jedlik.eu>
2024-02-15 10:32:37 +0100

    Version 1.7 Javítások

--- store/models.py
+++ store/models.py
@@ -104,7 +104,6 @@ class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     address = models.CharField(max_length=200, null = True)
-    house_number = models.IntegerField(null = True)
     city= models.CharField(max_length=200, null=True)
     state = models.CharField(max_length=200, null = True)
     zipcode = models.CharField(max_length=200, null = True)
