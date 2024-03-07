9b29c6834b250bd46bf2716d70719dec59079e62
kiss-csongor04 <kisscsongor@students.jedlik.eu>
2024-03-07 10:32:19 +0100

    Version 4.0 -  Egy új remény

--- store/models.py
+++ store/models.py
@@ -151,7 +151,7 @@ class BillingAddress(models.Model):
     
 class Post(models.Model):
     profile = models.ForeignKey(Customer, on_delete=models.DO_NOTHING, blank=True, null=True)
-    title = models.CharField(max_length=30, blank=True, null=True)
+    title = models.CharField(max_length=50, blank=True, null=True)
     body = models.CharField(max_length=1000)
     created = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(Customer, related_name='liked_by', symmetrical=False, blank=True)
